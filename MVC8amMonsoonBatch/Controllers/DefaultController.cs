823d59b6767bc19b1f7da323728ca11b35f686ca
yawar2019 <yawarali17@gmail.com>
2020-09-22 09:07:26 +0530

    updated code

--- MVC8amMonsoonBatch/Controllers/DefaultController.cs
+++ MVC8amMonsoonBatch/Controllers/DefaultController.cs
@@ -17,11 +17,13 @@ namespace MVC8amMonsoonBatch.Controllers
             return View();
         }
 
-        public ActionResult GetView(int id) {
+        public ActionResult GetView(int id)
+        {
             return View();
         }
 
-        public ActionResult Home() {
+        public ActionResult Home()
+        {
             return View();
         }
 
@@ -36,7 +38,8 @@ namespace MVC8amMonsoonBatch.Controllers
             return View();
         }
 
-        public ActionResult HtmlHelperExample() {
+        public ActionResult HtmlHelperExample()
+        {
             EmployeeModel obj = new EmployeeModel();
             obj.EmpName = "Abhishek Kumar";
             ViewBag.EmpList = new SelectList(db.employeeDetails, "EmpId", "EmpName");
@@ -44,7 +47,23 @@ namespace MVC8amMonsoonBatch.Controllers
         }
 
 
+        public ActionResult Index()
+        {
+            var emp = (from e in db.employeeDetails
+                       join
+                       d in db.Departments
+                       on e.DeptId
+                       equals d.DeptId
+                       select new empDept
+                       {
+                           EmpId = e.EmpId,
+                           EmpName = e.EmpName,
+                           EmpSalary = e.EmpSalary,
+                           DepartName = d.DeptName
+                       }).ToList();
 
+            return View(emp);
+        }
 
 
 
---
+++ MVC8amMonsoonBatch/Models/empDept.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC8amMonsoonBatch.Models
+{
+    public class empDept
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int? EmpSalary { get; set; }
+
+        public string DepartName { get; set; }
+    }
+}
